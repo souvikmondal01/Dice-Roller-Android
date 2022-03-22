f2efab0ea794a19044457451470ca19e240455e9
Souvik Mondal <souvikmondal2120@gmail.com>
2022-03-22 22:01:54 +0530

    Lottie animation added

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.diceroll
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.diceroll.databinding.ActivityMainBinding
 
@@ -15,11 +17,35 @@ class MainActivity : AppCompatActivity() {
         binding.tvNumber.text = viewModel.num.toString()
 
        binding.btnRoll.setOnClickListener {
+           binding.lavDice.visibility = View.VISIBLE
+           binding.tvNumber.visibility = View.INVISIBLE
+           binding.lavDice.playAnimation()
            binding.tvNumber.text = viewModel.num.toString()
            binding.tvNumber.text = viewModel.rollDice(6).toString()
 
+           binding.lavDice.addAnimatorListener(object : Animator.AnimatorListener {
+               override fun onAnimationStart(animator: Animator) {
+                   binding.lavDice.visibility = View.VISIBLE
+                   binding.tvNumber.visibility = View.INVISIBLE
+               }
+
+               override fun onAnimationEnd(animator: Animator) {
+                   binding.lavDice.visibility = View.GONE
+                   binding.tvNumber.visibility = View.VISIBLE
+               }
+
+               override fun onAnimationCancel(animator: Animator) {}
+               override fun onAnimationRepeat(animator: Animator) {}
+           })
+
+
        }
+
+
+
+
     }
 
 }
+
 
